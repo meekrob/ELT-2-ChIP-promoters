ffdb7a9605d2170e4489eea2917171750af8f774
David <davidcking.inbox@gmail.com>
2022-02-11 13:47:32 -0700

    Multiple files: Added deeptools analysis for just logFC > 0 vs logFC < 0

--- David/01_promoters/02_scripts/DEEPTOOLS.bash
+++ David/01_promoters/02_scripts/DEEPTOOLS.bash
@@ -6,7 +6,7 @@
 #                           -bs 100\
 #                           -R genes.classA.bed genes.classB.bed genes.classC.bed genes.classD.bed\
 
-if true
+if false
 then
     computeMatrix scale-regions --regionBodyLength 1200 \
                                 --startLabel 'up-1Kb' \
@@ -16,12 +16,34 @@ then
                                 -R promoters.hilo.classA.bed promoters.hilo.classC.bed promoters.hilo.classB.bed promoters.hilo.classD.bed\
                                 -S ELT2_LE_combined_subtracted.interp.bigWig\
                                 -p 4 -o promoters.olap100.hilo.mx
+
+    plotHeatmap  --matrixFile promoters.olap100.hilo.mx\
+                 -out promoters.olap100.hilo.pdf\
+                 --sortRegions no\
+                 --colorMap RdYlBu_r\
+                 --startLabel '' --endLabel ''\
+                 --regionsLabel 'peak+int. enrich.' 'peak+ NOT int. enrich.' 'NO peak + int. enrich.' 'NO peak + NOT int. enrich.'\
+                 --samplesLabel 'ELT-2 signal (reps. combined subtracted)'
 fi
 
-plotHeatmap  --matrixFile promoters.olap100.hilo.mx\
-             -out promoters.olap100.hilo.pdf\
-             --sortRegions no\
-             --colorMap RdYlBu_r\
-             --startLabel '' --endLabel ''\
-             --regionsLabel 'peak+int. enrich.' 'peak+ NOT int. enrich.' 'NO peak + int. enrich.' 'NO peak + NOT int. enrich.'\
-             --samplesLabel 'ELT-2 signal (reps. combined subtracted)'
+set -x
+if true
+then
+    computeMatrix scale-regions --regionBodyLength 1200 \
+                                --startLabel 'up-1Kb' \
+                                --endLabel down+200 \
+                                --beforeRegionStartLength 1000\
+                                --afterRegionStartLength 500\
+                                -R promoters.hilo.up.bed promoters.hilo.down.bed\
+                                -S ELT2_LE_combined_subtracted.interp.bigWig\
+                                -p 4 -o promoters.hilo.updown.mx
+
+    plotHeatmap  --matrixFile promoters.hilo.updown.mx\
+                 -out promoters.updown.pdf\
+                 --sortRegions no\
+                 --colorMap RdYlBu_r\
+                 --startLabel '' --endLabel ''\
+                 --regionsLabel 'log2FC > 0' 'log2FC < 0'\
+                 --samplesLabel 'ELT-2 signal (reps. combined subtracted)'
+fi
+
