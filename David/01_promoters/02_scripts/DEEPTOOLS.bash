d89a04cda6ded373a774639a32af5cf5bd68c31d
David <davidcking.inbox@gmail.com>
2022-02-22 14:48:27 -0700

    reset DEEPTOOLS.bash

--- David/01_promoters/02_scripts/DEEPTOOLS.bash
+++ David/01_promoters/02_scripts/DEEPTOOLS.bash
@@ -6,7 +6,25 @@
 #                           -bs 100\
 #                           -R genes.classA.bed genes.classB.bed genes.classC.bed genes.classD.bed\
 
+if false
+then
+    computeMatrix scale-regions --regionBodyLength 1200 \
+                                --startLabel 'up-1Kb' \
+                                --endLabel down+200 \
+                                --beforeRegionStartLength 1000\
+                                --afterRegionStartLength 500\
+                                -R promoters.hilo.classA.bed promoters.hilo.classC.bed promoters.hilo.classB.bed promoters.hilo.classD.bed\
+                                -S ELT2_LE_combined_subtracted.interp.bigWig\
+                                -p 4 -o promoters.olap100.hilo.mx
 
+    plotHeatmap  --matrixFile promoters.olap100.hilo.mx\
+                 -out promoters.olap100.hilo.pdf\
+                 --sortRegions no\
+                 --colorMap RdYlBu_r\
+                 --startLabel '' --endLabel ''\
+                 --regionsLabel 'peak+int. enrich.' 'peak+ NOT int. enrich.' 'NO peak + int. enrich.' 'NO peak + NOT int. enrich.'\
+                 --samplesLabel 'ELT-2 signal (reps. combined subtracted)'
+fi
 
 set -x
 if true
