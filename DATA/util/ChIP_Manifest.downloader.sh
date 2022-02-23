c65905b36764b209873b12b75ff9faf42ba56ce5
David <davidcking.inbox@gmail.com>
2022-02-22 18:11:05 -0700

    improvements to downloader

--- DATA/util/ChIP_Manifest.downloader.sh
+++ DATA/util/ChIP_Manifest.downloader.sh
@@ -3,8 +3,16 @@
 
 skip_header=1
 
+function debug {
+    continue
+    echo -e  $@ >&2
+}
+
+linenum=0
 while IFS=$'\t' read -r -a x; 
 do 
+    linenum=$((linenum+1))
+
     if [ "$skip_header" -eq 1 ]
     then
         skip_header=0
@@ -13,11 +21,45 @@ do
         
     if [ -z "$x" ]
     then
+        debug "Skipping blank line $linenum"
         continue
     fi
+
+    if [ ${#x[@]} -lt 6 ]
+    then
+        debug "Error for line $linenum: not enough fields"
+        continue
+    fi
+
     url="${x[6]}"
+
+    if [ -z "$url" ]
+    then
+        debug "Error for line $linenum: URL missing for '$filename'"
+        debug "0: ${x[0]}"
+        debug "1: ${x[1]}"
+        debug "2: ${x[2]}"
+        debug "3: ${x[3]}"
+        debug "4: ${x[4]}"
+        debug "5: ${x[5]}"
+        debug "6: ${x[6]}"
+    fi
+
+    echo -e "URL:\t$url" 
+
     filename="${x[5]}"
-    cmd="wget -O $filename $url"
-    echo $cmd
-    eval $cmd
+    echo -e "FNAME:\t$filename" 
+
+    if [ -z "$filename" ]
+    then
+        debug "Error for line $linenum: Missing filename $filename"
+        continue
+    fi
+    if ! [ -e "$filename" ]
+    then
+        cmd="wget -O $filename $url"
+        echo "Running: $cmd"
+        eval $cmd
+    fi
+    echo
 done < <(util/exportXLsheet.RScript ChIP_Source_Data_Manifest.xlsx)
